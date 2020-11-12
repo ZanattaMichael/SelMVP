7d9a99f8787d6a104a57a2c765b38d6bcbb61c1a
Michael Zanatta <zanattaam@gmail.com>
2020-11-12 16:35:20 +1000

    Updated to Support GHA

--- Tests/Invoke-Tests.ps1
+++ Tests/Invoke-Tests.ps1
@@ -1,8 +1,19 @@
-# Invoke the Loader
-. ..\Build\LocalLoader.ps1
+param($RootPath)
+
+$Location = Split-Path -Path (Get-Location) -Leaf
+# Is the Executing Path of the Script inside the Tests Directory
+if ($Location -eq 'Tests') {
+    # Update the Root Path
+    $RootPath = Split-Path -Path (Get-Location) -Parent
+}
+
+$UpdatedPath = Join-Path -Path $RootPath -ChildPath 'build\LocalLoader.ps1' 
+
+# Set a Script Variable that sets the tests root path. This is used in Mocking with HTML
+$Script:TestRootPath = Join-Path -Path $RootPath -ChildPath "Tests"
 
 # Set the FilePath
-$Script:TestRootPath = Split-Path $MyInvocation.MyCommand.Path -Parent
+. $UpdatedPath
 
 # Invoke the Pester Tests
 Invoke-Pester -Path 'Private'
