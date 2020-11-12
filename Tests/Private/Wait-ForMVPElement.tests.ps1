00a0b20ba1ee129de4bfcda91b575759e26cfa4f
Michael Zanatta <zanattaam@gmail.com>
2020-11-12 12:28:27 +1000

    Adding Wait-ForMVPElement Unit Tests

---
+++ Tests/Private/Wait-ForMVPElement.tests.ps1
@@ -0,0 +1,48 @@
+Describe "Wait-ForMVPElement" {
+
+    BeforeAll {
+        $Global:MVPDriver = [PSCustomObject]@{
+            Name = "Data"
+        }
+    }
+
+    it "Standard Execution" {
+
+        Mock -CommandName Enter-SeUrl -MockWith {}
+        Mock -CommandName Find-SeElement -MockWith {
+            [PSCustomObject]@{
+                Data = 'Test'
+            }
+        }
+        Mock -CommandName Start-Sleep -MockWith {}
+        Mock -CommandName Write-Debug -MockWith {}
+        Mock -CommandName Write-Warning -MockWith {}
+
+        Wait-ForMVPElement
+
+        Should -Invoke Find-SeElement -Exactly 1
+        Should -Invoke Write-Debug -Exactly 1
+        Should -Invoke Write-Error -Exactly 0
+        Should -Invoke Enter-SeUrl -Exactly 0
+
+    }
+
+    it "Where it times out" {
+
+        Mock -CommandName Enter-SeUrl -MockWith {}
+        Mock -CommandName Find-SeElement -MockWith {}
+        Mock -CommandName Start-Sleep -MockWith {}
+        Mock -CommandName Write-Debug -MockWith {}
+        Mock -CommandName Write-Warning -MockWith {}
+        Mock -CommandName Write-Error -MockWith {}
+
+        Wait-ForMVPElement
+
+        Should -Invoke Write-Error -Exactly 1
+        Should -Invoke Write-Debug -Exactly 11
+        Should -Invoke Enter-SeUrl -Exactly 10
+        Should -Invoke Write-Error -Exactly 1
+
+    }
+
+}
