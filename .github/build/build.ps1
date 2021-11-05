7adf25106668b78ddae3802c6d67aa3ce168c62f
Michael Zanatta <zanattaam@gmail.com>
2021-11-06 07:59:14 +1000

    Adjusting Yaml Files Adding Scripts for Choco Install

---
+++ .github/build/build.ps1
@@ -0,0 +1,1 @@
+pwsh -file .\Build\BuildModule.ps1
---
+++ Build/BuildChocoPackage.ps1
@@ -0,0 +1,51 @@
+#Requires -Version 7.0
+
+Write-Verbose "Building PowerShell Chocolatey Package:"
+
+$BuildDirectory = Split-Path -parent $PSCommandPath
+$BuildModuleDirectory = [System.IO.Path]::Join($BuildDirectory, "SelMVP")
+$BuildModuleFile = [System.IO.Path]::Join($BuildModuleDirectory, "Module.psm1")
+$BuildModulePSDFile = [System.IO.Path]::Join($BuildModuleDirectory, "SelMVP.psd1")
+$BuildModuleDirectoryLibraries = [System.IO.Path]::Join($BuildModuleDirectory)
+
+$ErrorActionPreference = 'Stop'
+
+#
+# Test that the Build is Present
+
+$PreReqPaths = @(
+    $BuildModuleDirectory
+    $BuildModuleFile
+    $BuildModulePSDFile
+    $BuildModuleDirectoryLibraries
+)
+
+Write-Verbose "Testing: $($PreReqPaths -join ', ')"
+
+if ((Test-Path $PreReqPaths) -contains $false) { Throw "Build was not not started or was incomplete!" }
+
+#
+# Start Building the Choco Package
+
+# Parse the Manifest in here!
+$manifest = . $BuildModulePSDFile
+
+# Format our Parameters
+$parameters = @{
+    maintainername = "Michael Zanatta"
+    maintainerrepo = "https://github.com/ZanattaMichael/SelMVP"
+    packageversion = $manifest.ModuleVersion\
+    installertype = 'zip'
+    url =
+    url64 =
+}
+
+choco new SelMVP maintainername="Michael Zanatta" 
+
+packageversion
+maintainername
+maintainerrepo
+installertype
+url
+url64
+silentargs
